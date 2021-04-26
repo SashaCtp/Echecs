d1dab355ccf0b254952fc2601cac3284b183d967
Sasha Contrepois <sasha.contrepois@hotmail.fr>
2021-04-26 22:26:01 +0200

    Ajout de la classe Game

---
+++ src/game/Game.java
@@ -0,0 +1,33 @@
+package game;
+
+import game.interfaces.*;
+
+public class Game {
+
+    private int currentPlayerIndex;
+    private IPlayer[] players;
+
+    private Chessboard chessboard;
+
+    // Factories
+    private PieceFactory pieceFactory;
+    private PlayerFactory playerFactory;
+    /**
+     * Constructor
+     * @param pieceFact Fabrique de pièces d'échec
+     * @param playerFact Fabrique de joueur
+     * @param chessboardFact Fabrique d'échiquier
+     */
+    public Game(PieceFactory pieceFact, PlayerFactory playerFact, ChessboardFactory chessboardFact){
+
+        this.pieceFactory = pieceFact;
+        this.playerFactory = playerFact;
+        this.chessboard = chessboardFact.newChessboard();
+
+    }
+
+    public static Color getOpponentColor(Color c){
+        return c.equals(Color.WHITE) ? Color.BLACK : Color.WHITE;
+    }
+
+}
